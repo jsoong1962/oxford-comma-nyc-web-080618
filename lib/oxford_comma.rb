cc88a96c361776895fa358e3e1bd5888bb49ddce
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 21:30:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
  last_string =array.insert(-2, "and")
  last = last_string.slice(-2, last_string.length)
  final = last.join(" ")
- first_string + " " + final
+ first_string + ", " + final
 end
 
 p oxford_comma(["fiddleheads","okra","kohlrabi"])
