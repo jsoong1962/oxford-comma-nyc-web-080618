876503ea627405714a6a081f428e7e38a8d5f3db
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 21:35:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   array[0]
+ elsif array.length == 2
+ add = array.insert(-2, "and")
+ add.join(" ")
+ else
+ first = array.slice(0, array.length - 1) 
+ first_string = first.join(", ")
+ last_string =array.insert(-2, "and")
+ last = last_string.slice(-2, last_string.length)
+ final = last.join(" ")
+ first_string + ", " + final
+ end
 end
+
+p oxford_comma(["fiddleheads","okra","kohlrabi"])
