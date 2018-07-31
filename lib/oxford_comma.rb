a503446b3c5b9ebece13aeceb0925c5aff408a0d
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 21:34:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
  if array.length == 1
    array[0]
+ elsif array.length == 2
+ add = array.insert(-2, "and")
+ add.join(" ")
  else
  first = array.slice(0, array.length - 1) 
  first_string =first.join(", ")
